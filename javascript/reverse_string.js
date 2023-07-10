365ea6af227b80c7527170b74633c24a067d8a3b
Dustin <dustinhollowaypdx@gmail.com>
2023-07-10 13:32:51 -0700

    added two versions of algo

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,17 @@
 function reverseString(str) {
+  // the reverse method is built in and looks like this
+  // return str.split("").reverse().join("")
   // type your code here
+
+  // using a for loop to iterate over each index of str and adding it to the empty value of reversed.
+
+  reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+
+  return reversed;
 }
 
 if (require.main === module) {
